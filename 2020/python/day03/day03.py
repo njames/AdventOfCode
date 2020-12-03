9b311ca360eb28f02d4b5cc87106054cf53d0d97
Nigel James <nigel.james@squarecloud.com.au>
2020-12-03 20:40:06 +1100

    day 3 2020 in the bag with 2 stars to my name

--- 2020/python/day03/day03.py
+++ 2020/python/day03/day03.py
@@ -1,9 +1,18 @@
 import re
 import time
 
+def check_slope(data, slope):
+    cols = len(data[0])
+    rows = len(data)
+    r = c = 0
+    trees = 0
+    while r < rows:
+        if data[r][c] == '#':
+            trees += 1
+        r += slope[0]
+        c = (c + slope[1]) % cols
 
-
-
+    return trees
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
@@ -13,24 +22,26 @@ if __name__ == '__main__':
     t = time.perf_counter()
     # first thoughts
     # get the length of the line and treat this as a modular arithmetic to wrap around when counting to the right
-    trees = 0
-    cols = len(data[0])
-    rows = len(data)
-    r = c = 0
-    while r < rows:
-        if data[r][c] == '#':
-            trees += 1
-        r += 1
-        c = (c + 3) % cols
+    trees = check_slope(data, [1, 3])
+    print(f'Part 1: The number of trees encountered  is {trees}')
 
+    trees = 1
 
+    slopes = [
+        [1, 1],
+        [1, 3],
+        [1, 5],
+        [1, 7],
+        [2, 1],
+    ]
 
+    for slope in slopes:
+        trees *= check_slope(data, slope)
 
     # print(f'line len {length} rows {rows}')
     # print(data)
 
-    print(f'Part 1: The number of trees encountered  is {trees}')
-    print(f'Part 2: The number of correct policies is {0}')
+    print(f'Part 2: The number of multiplied trees is {trees}')
     print(f'Time taken {time.perf_counter() - t} seconds')
 
     # part two
