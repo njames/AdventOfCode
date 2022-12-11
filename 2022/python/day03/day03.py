9774bb7a9e6c97e0aa7a65c8cf81489950971b06
Nigel James <nigel.james@gmail.com>
2022-12-11 21:18:07 +1100

    advent of code 2022 day 03 part 2 python

--- 2022/python/day03/day03.py
+++ 2022/python/day03/day03.py
@@ -14,22 +14,31 @@ def common(rucksacks):
     return list(filter(lambda x: x in rucksacks[0], rucksacks[1]))[0]
 
 
+def commonSet(r1, r2):
+    return set(filter(lambda x: x in r1, r2))
+
+
 def splitLine(line=None):
     return [line[0:int(len(line) / 2)], line[int(len(line) / 2):]]
 
 
 if __name__ == '__main__':
     # get input
-    # data = [score(common(splitLine(line))) for line in open('../../input/day03/test.txt').read().strip().split('\n')]
-    data = [score(common(splitLine(line))) for line in open('../../input/day03/input03.txt').read().strip().split('\n')]
+    # data = [line for line in open('../../input/day03/test.txt').read().strip().split('\n')]
+    data = [line for line in open('../../input/day03/input03.txt').read().strip().split('\n')]
+
+    part1 = []
+    for line in data:
+        part1.append(score(common(splitLine(line))))
 
     print('\n')
-    print('Part 1', sum(data))
+    print('Part 1', sum(part1))
+
+    part2 = []
+
+    for i in range(0, len(data), 3):
+        part2.append(score(commonSet(data[i], data[1 + i]).intersection(commonSet(data[1 + i], data[2 + i])).pop()))
+
+    print('Part 2', sum(part2))
 
     t = time.perf_counter()
-    # part one
-    # print( ' Part 1 ', max(sums))
-
-    # part two
-    # sums.sort(reverse=True)
-    # print('Part 2 ', sum(sums[0:3]))
