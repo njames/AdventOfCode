c34fe615b6acb756f9d7e171fcaf6949f9c47e5d
Nigel James <nigel.james@gmail.com>
2022-12-11 20:30:53 +1100

    advent of code 2022 day 03 part 1 python

--- 2022/python/day03/day03.py
+++ 2022/python/day03/day03.py
@@ -1,49 +1,30 @@
 import time
 from typing import List, Any
 
+
 # Press the green button in the gutter to run the script.
+def score(c):
+    value = ord(c) - 96
+    if value < 0:
+        value = ord(c) - ord('A') + 27
+    return value
+
+
+def common(rucksacks):
+    return list(filter(lambda x: x in rucksacks[0], rucksacks[1]))[0]
+
+
+def splitLine(line=None):
+    return [line[0:int(len(line) / 2)], line[int(len(line) / 2):]]
 
 
 if __name__ == '__main__':
     # get input
-    # data = [line.split(' ') for line in open('../../input/day02/test.txt').read().strip().split('\n')]
-    data = [line.split(' ') for line in open('../../input/day02/input02.txt').read().strip().split('\n')]
+    # data = [score(common(splitLine(line))) for line in open('../../input/day03/test.txt').read().strip().split('\n')]
+    data = [score(common(splitLine(line))) for line in open('../../input/day03/input03.txt').read().strip().split('\n')]
 
-    scores: list[int] = []
-    scores2: list[int] = []
-    for game in data:
-        o = ord(game[0]) - ord('A')
-        m = ord(game[1]) - ord('X')
-
-        # equal
-        if o == m:
-            scores.append(m + 1 + 3)
-            print(o, m, 'draw')
-        #     m wins
-        elif ((o + 1) % 3) == m:
-            scores.append(m + 1 + 6)
-            print(o, m, 'win')
-        else:
-            scores.append(m + 1)
-            print(o, m, 'loss')
-
-    print('part 2 ',sum(scores))
-
-    for game in data:
-        o = ord(game[0]) - ord('A')
-        m = ord(game[1]) - ord('X')
-
-        if m == 0: # lose
-            scores2.append(((o - 1) % 3) + 1)
-        elif m == 1: # draw
-            scores2.append(o + 1 + 3)
-        else: # win
-            scores2.append(((o + 1) % 3) + 1 + 6)
-
-    print('part 2 ',sum(scores2))
-
-    # print('\n')
-    # print(scores)
+    print('\n')
+    print('Part 1', sum(data))
 
     t = time.perf_counter()
     # part one
